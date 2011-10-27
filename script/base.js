e2b08020ae64e9bbd98c63145c0a862e2bcc7829
WolfieZero <neil@wolfiezero.com>
2011-10-27 10:35:19 +0100

    View Templatie

---
+++ script/base.js
@@ -0,0 +1,23 @@
+/**
+ * Main site scripts 
+ */
+$(document).ready(function() {
+	$('textarea').autoGrow();
+
+	log('Welcome to Templatie! Enjoy your stay but please leave the carrot cake alone...');
+});
+
+/**
+ * Is Set
+ * Checks element exists on a page
+ * 
+ * @param	element
+ * @return	bool
+ *****************************************************************************/
+function isset(elem) {
+	if ( $(elem).length >= 1 ) {
+		return true;
+	} else {
+		return false;
+	}
+}
